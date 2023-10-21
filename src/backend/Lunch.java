2089ee4f491441691fc5701c00e91e10da2352b1
p3rseph0ne <miriam.hauer.33@gmail.com>
2023-10-21 16:51:50 +0200

    added check opening day logic for restaurants

--- src/backend/Lunch.java
+++ src/backend/Lunch.java
@@ -1,6 +1,5 @@
 package backend;
 
-import java.util.Date;
 import java.util.List;
 
 public class Lunch {
@@ -9,21 +8,19 @@ public class Lunch {
     private List<Customer> customs;
     private List<String> preferences;
     private List<String> allergies;
-    private Date today;
+    private String today;
     private String weather;
     private List<Restaurant> allRestaurants;
     private List<Restaurant> availableRestaurants;
 
-    public Lunch(List<Employee> emps, List<Customer> customs, Date today, String weather) {
+    public Lunch(List<Employee> emps, List<Customer> customs, String today, String weather) {
         this.emps = emps;
         this.customs = customs;
         this.today = today;
         this.weather = weather;
 
-        //iterate over customer and employee to initialize allergies and preferences
-
         readRestaurants();
-        readAllergies(emps,customs);
+        readAllergiesAndPreferences(emps,customs);
         defineAvailableRestaurants(today);
     }
 
@@ -31,19 +28,33 @@ public class Lunch {
         return availableRestaurants;
     }
 
-    private void readAllergies(List<Employee> emps, List<Customer> customs){
-        //iterate over allergies of Persons, checking if allergie is already on list or not
-
+    private void readAllergiesAndPreferences(List<Employee> emps, List<Customer> customs){
+        for (Customer c:customs) {
+            for(String s:c.getAllergies()){
+                if(!allergies.contains(s)) allergies.add(s);
+            }
+            for(String s:c.getPreferences()){
+                if(!preferences.contains(s)) preferences.add(s);
+            }
+        }
+        for (Employee e:emps) {
+            for(String s:e.getAllergies()){
+                if(!allergies.contains(s)) allergies.add(s);
+            }
+            for(String s:e.getPreferences()){
+                if(!preferences.contains(s)) preferences.add(s);
+            }
+        }
     }
 
     private void readRestaurants(){
         //call dom implementation that reads a file with restaurant data
     }
-    public List<Restaurant> defineAvailableRestaurants(Date today){
+    public List<Restaurant> defineAvailableRestaurants(String today){
         //
         for(int i = 0;i < allRestaurants.size(); i++){
             Restaurant currRest = allRestaurants.get(i);
-            //check opening hours
+            //check opening days
             if(currRest.isOpen(today)){
                 //check allergies
                 for(int j = 0; j < currRest.getMenu().getDishes().size(); j++){
--- src/backend/Restaurant.java
+++ src/backend/Restaurant.java
@@ -1,6 +1,5 @@
 package backend;
 
-import java.util.Date;
 import java.util.List;
 
 public class Restaurant {
@@ -10,15 +9,15 @@ public class Restaurant {
     //range 1-3 cheap - expensive
     private int priceRange;
     //is list the best option? 2D array?
-    private List<String[]> openingHours;
+    private List<String> openingDays;
     private boolean hasTerrace;
     private Menu menu;
 
-    public Restaurant(String name, String address, int priceRange, List<String[]> openingHours, boolean hasTerrace, Menu menu) {
+    public Restaurant(String name, String address, int priceRange, List<String> openingDays, boolean hasTerrace, Menu menu) {
         this.name = name;
         this.address = address;
         this.priceRange = priceRange;
-        this.openingHours = openingHours;
+        this.openingDays = openingDays;
         this.hasTerrace = hasTerrace;
         this.menu = menu;
     }
@@ -47,15 +46,15 @@ public class Restaurant {
         this.priceRange = priceRange;
     }
 
-    public List<String[]> getOpeningHours() {
-        return openingHours;
+    public List<String> getopeningDays() {
+        return openingDays;
     }
 
-    public void setOpeningHours(List<String[]> openingHours) {
-        this.openingHours = openingHours;
+    public void setopeningDays(List<String> openingDays) {
+        this.openingDays = openingDays;
     }
 
-    public boolean isHasTerrace() {
+    public boolean HasTerrace() {
         return hasTerrace;
     }
 
@@ -71,9 +70,11 @@ public class Restaurant {
         this.menu = menu;
     }
 
-    public boolean isOpen(Date today){
-        //implement comparison today and openinghourse
-        return true;
+    public boolean isOpen(String today){
+
+        if(openingDays.contains(today)) return true;
+
+        return false;
     }
     @Override
     public String toString() {
