0ec175f86668c6b2103467c186b7a3c7052c645f
miriam <miriam.hauer.33@gmail.com>
2023-10-28 13:52:21 +0200

    added teststuff

---
+++ src/backend/businesslogic/LunchTest.java
@@ -0,0 +1,20 @@
+package backend.businesslogic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LunchTest {
+
+    @Test
+    void getAvailableRestaurants() {
+    }
+
+    @Test
+    void defineAvailableRestaurants() {
+    }
+
+    @Test
+    void randomRestaurant() {
+    }
+}
